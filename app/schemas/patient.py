d66f9660adb11eec37371533bdffdae1858b128f
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-09-10 01:09:58 +0800

    response_model added

--- app/schemas/patient.py
+++ app/schemas/patient.py
@@ -9,17 +9,17 @@ from ..utils.schemaHelper import Base, as_form
 class PatientBase(Base):
     type : str
     first_name : str
-    middle_name : Optional[str]
+    middle_name : Optional[str] = None
     last_name : str
-    suffix_name : Optional[str]
+    suffix_name : Optional[str] = None
     birth_date : date
     gender : str
     contact_no : str
     email : str
     blood_type : str
-    picture : Optional[str]
+    picture : Optional[str] = None
 
-    is_active: Optional[str]
+    is_active: Optional[str] = None
 
 
 @as_form
@@ -28,10 +28,16 @@ class CreatePatient(PatientBase):
 
 
 class Patient(PatientBase):
-    created_at: Optional[dt]
-    updated_at: Optional[dt]
+    id: str
+    created_at: Optional[dt] = None
+    updated_at: Optional[dt] = None
 
-class PatientOut(Base):
+class OutPatients(Base):
     data: List[Patient]
+    error: bool
+    message: str
+
+class OutPatient(Base):
+    data: Patient
     error: bool
     message: str
