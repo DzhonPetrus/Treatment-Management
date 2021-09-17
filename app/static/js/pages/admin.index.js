13c2681fda7756662b144b62d5336f3485c8e9ee
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-09-17 17:37:39 +0800

    dashboard in admin done

--- app/static/js/pages/admin.index.js
+++ app/static/js/pages/admin.index.js
@@ -21,7 +21,8 @@ $(function () {
 			const totalUsersActive = active_users.length;
 			const totalUsersInactive = inactive_users.length;
 
-			let chartCanvas = $('#usersChart').get(0).getContext('2d')
+			$('#totalUsers').html(totalUsers);
+			let chartCanvas = $('#usersChart').get(0).getContext('2d');
 			let chartData = {
 			  labels: [
 				  'Active Users',
@@ -64,6 +65,31 @@ $(function () {
 			const total_active_patients = active_patients.length;
 			const total_inactive_patients = inactive_patients.length;
 
+			$('#totalPatients').html(total_patients);
+			let chartCanvas = $('#patientsChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Patients',
+				  'Inactive Patients',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_patients, total_inactive_patients],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'doughnut',
+			  data: chartData,
+			  options: chartOptions
+			})
+
+
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
 	});
@@ -79,11 +105,36 @@ $(function () {
 		success: data => {
 			const treatment_types = data.data;
 			const active_treatment_types = treatment_types.filter(treatment_type => treatment_type.is_active === 'ACTIVE')
-			const inactive_treatments = treatments.filter(treatment_type => treatment_type.is_active === 'INACTIVE')
+			const inactive_treatment_types = treatment_types.filter(treatment_type => treatment_type.is_active === 'INACTIVE')
 
 			const total_treatment_types = treatment_types.length;
 			const total_active_treatment_types = active_treatment_types.length;
 			const total_inactive_treatment_types = inactive_treatment_types.length;
+
+			$('#totalTreatmentTypes').html(total_treatment_types);
+			let chartCanvas = $('#treatmentTypeChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Treatment Type',
+				  'Inactive Treatment Type',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_treatment_types, total_inactive_treatment_types],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'pie',
+			  data: chartData,
+			  options: chartOptions
+			})
+
 
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
@@ -106,6 +157,31 @@ $(function () {
 			const total_active_lab_requests = active_lab_requests.length;
 			const total_inactive_lab_requests = inactive_lab_requests.length;
 
+			$('#totalLabRequests').html(total_lab_requests);
+			let chartCanvas = $('#labRequestChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Lab Request',
+				  'Inactive Lab Request',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_lab_requests, total_inactive_lab_requests],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'doughnut',
+			  data: chartData,
+			  options: chartOptions
+			})
+
+
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
 	});
@@ -127,6 +203,31 @@ $(function () {
 			const total_active_lab_tests = active_lab_tests.length;
 			const total_inactive_lab_tests = inactive_lab_tests.length;
 
+			$('#totalLabTest').html(total_lab_tests);
+			let chartCanvas = $('#labTestChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Lab Test',
+				  'Inactive Lab Test',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_lab_tests, total_inactive_lab_tests],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'pie',
+			  data: chartData,
+			  options: chartOptions
+			})
+
+
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
 	});
@@ -141,12 +242,37 @@ $(function () {
 
 		success: data => {
 			const surgeries = data.data;
-			const active_surgeries = surgery.filter(surgery => surgery.is_active === 'ACTIVE')
-			const inactive_surgeries = surgery.filter(surgery => surgery.is_active === 'INACTIVE')
+			const active_surgeries = surgeries.filter(surgery => surgery.is_active === 'ACTIVE')
+			const inactive_surgeries = surgeries.filter(surgery => surgery.is_active === 'INACTIVE')
 
 			const total_surgeries = surgeries.length;
 			const total_active_surgeries = active_surgeries.length;
 			const total_inactive_surgeries = inactive_surgeries.length;
+
+			$('#totalSurgeries').html(total_surgeries);
+			let chartCanvas = $('#surgeriesChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Surgery',
+				  'Inactive Surgery',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_surgeries, total_inactive_surgeries],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'doughnut',
+			  data: chartData,
+			  options: chartOptions
+			})
+
 
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
@@ -168,6 +294,31 @@ $(function () {
 			const total_surgery_types = surgery_types.length;
 			const total_active_surgery_types = active_surgery_types.length;
 			const total_inactive_surgery_types = inactive_surgery_types.length;
+
+			$('#totalSurgeryTypes').html(total_surgery_types);
+			let chartCanvas = $('#surgeryTypeChart').get(0).getContext('2d');
+			let chartData = {
+			  labels: [
+				  'Active Surgery Type',
+				  'Inactive Surgery Type',
+			  ],
+			  datasets: [
+				{
+				  data: [total_active_surgery_types, total_inactive_surgery_types],
+				  backgroundColor : ['#f56954', '#00a65a'],
+				}
+			  ]
+			}
+			let chartOptions = {
+			  maintainAspectRatio : false,
+			  responsive : true,
+			}
+			new Chart(chartCanvas, {
+			  type: 'pie',
+			  data: chartData,
+			  options: chartOptions
+			})
+
 
 		},
 		error: (data) => notification("error", data.responseJSON.detail),
