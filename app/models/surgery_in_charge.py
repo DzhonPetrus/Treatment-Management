ce3170724072e05f4bb7047e455f987a2300f5b8
John Peter Narzoles <dzhonpetrus@gmail.com>
2022-02-18 15:25:53 +0800

    ids to FK

--- app/models/surgery_in_charge.py
+++ app/models/surgery_in_charge.py
@@ -8,11 +8,12 @@ from ..database import Base
 class SurgeryInCharge(Base):
     __tablename__ = "surgery_in_charge"
 
+    id = Column(String(36), primary_key=True, default=text('UUID()'))
     professional_fee = Column(Numeric(15,2))
 
 
-    in_charge_id = Column(ForeignKey('users.id'), primary_key=True)
-    surgery_id = Column(ForeignKey('surgeries.id'), primary_key=True)
+    in_charge_id = Column(ForeignKey('users.id'))
+    surgery_id = Column(ForeignKey('surgeries.id'))
 
     in_charge = relationship('User', back_populates="handled_surgeries")
     surgery = relationship("Surgery", back_populates="in_charge")
